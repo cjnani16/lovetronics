0ab308d4856323cf5bc64d4358879d37047cfbfc
Chad Turner <chadwickpturner@gmail.com>
2020-02-01 12:32:46 -0800

    Add basic tab functionality

---
+++ Assets/Scripts/TabManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabManager : MonoBehaviour
+{
+    public List<GameObject> pages;
+    public List<GameObject> tabs;
+
+    public int selectedTab = 0; // 0 = chassis, 1 = leg, 2 = equip
+
+    void Start()
+    {
+        setSelectedTabColor();
+    }
+
+    public void swapTab(int newTab)
+    {
+        selectedTab = newTab;
+        setSelectedTabColor();
+        for (int i = 0; i < pages.Count; i++)
+        {
+            if (i == selectedTab)
+            {
+                pages[i].SetActive(true);
+            }
+            else
+            {
+                pages[i].SetActive(false);
+            }
+        }
+    }
+
+    private void setSelectedTabColor()
+    {
+        for (int i = 0; i < tabs.Count; i++)
+        {
+            if (i == selectedTab)
+            {
+                tabs[i].GetComponent<Image>().color = new Color32(140, 200, 200, 255);
+            }
+            else
+            {
+                tabs[i].GetComponent<Image>().color = Color.white;
+            }
+        }
+    }
+
+}
