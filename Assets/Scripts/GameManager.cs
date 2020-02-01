60d79e74943157e882ab8334205d77fca993c422
Chad Turner <chadwickpturner@gmail.com>
2020-02-01 10:57:33 -0800

    base game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager = null;
+    private PlayerStats baseStats;
+
+    void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+        }
+        else if (manager != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        baseStats = new PlayerStats(10, 30, 120, 50, 5);
+    }
+
+    public PlayerStats getBaseStats()
+    {
+        return baseStats;
+    }
+
+    public void setBaseStats(PlayerStats stats)
+    {
+        baseStats = stats;
+    }
+}
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,7 +6,39 @@ public class PlayerStats
 {
     private int attack;
     private int defense;
-    private int health;
+    private int maxHealth;
     private int maxCoolant;
     private int coolantRegen;
+
+    public PlayerStats(int atk, int def, int hp, int cool, int regen)
+    {
+        attack = atk;
+        defense = def;
+        maxHealth = hp;
+        maxCoolant = cool;
+        coolantRegen = regen;
+    }
+
+    public int getAttack()
+    {
+        return attack;
+    }
+    public int getDefense()
+    {
+        return defense;
+    }
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+    public int getMaxCoolant()
+    {
+        return maxCoolant;
+    }
+    public int getCoolantRegen()
+    {
+        return coolantRegen;
+    }
+
+
 }
