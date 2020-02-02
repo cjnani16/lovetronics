ab10fc553a006c502c29afa83c68f20954c7f115
Jñani Crawford <crawford.jnani@gmail.com>
2020-02-02 02:28:14 -0800

    Suppress null errors in inventory screen

--- Assets/Scripts/PlayerImageController.cs
+++ Assets/Scripts/PlayerImageController.cs
@@ -20,7 +20,8 @@ public class PlayerImageController : MonoBehaviour
     private void updateImage()
     {
         Image img = GetComponent<Image>();
-        img.sprite = GameManager.manager.getSprite();
+        Sprite defaultSprite = img.sprite;
+        img.sprite = (GameManager.manager.getSprite()!=null)?GameManager.manager.getSprite():defaultSprite;
         // transform.scale.x = 0.9;
         // transform.scale.y = 0.9;
         GetComponent<RectTransform>().sizeDelta = Vector2.Scale(GetComponent<Image>().sprite.rect.size, new Vector2(0.85f, 0.85f));
