f77a56e577b98035e216749358f810e0aeb94e5d
Chad Turner <chadwickpturner@gmail.com>
2020-02-01 22:30:50 -0800

    Add lots o' audio

--- Assets/Scripts/HardwareRowController.cs
+++ Assets/Scripts/HardwareRowController.cs
@@ -26,6 +26,7 @@ public class HardwareRowController : MonoBehaviour
 
     public void setSelectedHardware()
     {
+        AudioManager.instance.Play("select");
         switch (hardware.category)
         {
             case Hardware.Category.CHASSIS:
