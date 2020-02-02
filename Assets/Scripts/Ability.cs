99138a98fdfa50ce01353521bfe5beda11bca710
Chad Turner <chadwickpturner@gmail.com>
2020-02-02 14:04:24 -0800

    Fix sound effects in battle

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -10,33 +10,38 @@ public class BuffDebuff
     private PlayerStats statChanges;
     private int duration;
 
-    public BuffDebuff(string n, PlayerStats s, int d) {
+    public BuffDebuff(string n, PlayerStats s, int d)
+    {
         this.name = n;
         this.statChanges = s;
         this.duration = d;
-        this.isHealBleed=false;
+        this.isHealBleed = false;
     }
 
     //heal or bleed (dot or instant)
-    public BuffDebuff(string n, float amount, int d) {
+    public BuffDebuff(string n, float amount, int d)
+    {
         this.name = n;
-        this.statChanges = new PlayerStats(0,0,0,0,0);
+        this.statChanges = new PlayerStats(0, 0, 0, 0, 0);
         this.healBleedAmt = amount;
-        this.isHealBleed=true;
+        this.isHealBleed = true;
         this.duration = d;
     }
 
-    public PlayerStats GetStatChanges() {
+    public PlayerStats GetStatChanges()
+    {
         return statChanges;
     }
 
     //lower timer and return true if expired.
-    public void decrement() {
+    public void decrement()
+    {
         duration--;
     }
 
-    public bool isExpired() {
-        return duration<=0;
+    public bool isExpired()
+    {
+        return duration <= 0;
     }
 }
 public struct Ability
@@ -47,7 +52,8 @@ public struct Ability
     public List<BuffDebuff> appliedEffects;
     public List<BuffDebuff> enemyAppliedEffects;
 
-    public Ability(string name, string description, float power, float cost, string sound) {
+    public Ability(string name, string description, float power, float cost, string sound)
+    {
         this.name = name;
         this.description = description;
         this.power = power;
@@ -56,36 +62,47 @@ public struct Ability
         this.enemyAppliedEffects = new List<BuffDebuff>();
         this.sound = sound;
     }
-    public void AddEffect(string name, PlayerStats statChange, int duration) {
+    public void AddEffect(string name, PlayerStats statChange, int duration)
+    {
         this.appliedEffects.Add(new BuffDebuff(name, statChange, duration));
-        this.description+= ("\n"+statChange.stringify());
+        this.description += ("\n" + statChange.stringify());
     }
 
-    public void AddEffect(BuffDebuff e) {
+    public void AddEffect(BuffDebuff e)
+    {
         this.appliedEffects.Add(e);
-        this.description+= ("\n"+e.GetStatChanges().stringify());
+        this.description += ("\n" + e.GetStatChanges().stringify());
     }
 
-    public void AddEnemyEffect(string name, PlayerStats statChange, int duration) {
+    public void AddEnemyEffect(string name, PlayerStats statChange, int duration)
+    {
         this.enemyAppliedEffects.Add(new BuffDebuff(name, statChange, duration));
-        this.description+= ("\nTo enemy:"+statChange.stringify());
+        this.description += ("\nTo enemy:" + statChange.stringify());
     }
 
-    public void AddEnemyEffect(BuffDebuff e) {
+    public void AddEnemyEffect(BuffDebuff e)
+    {
         this.enemyAppliedEffects.Add(e);
-        this.description+= ("\nTo enemy:"+e.GetStatChanges().stringify());
+        this.description += ("\nTo enemy:" + e.GetStatChanges().stringify());
     }
 
     //for something more complex than a stat buff/debuff
-    public void ApplyOnCastSpecialEffect(ref BattlerState user, ref BattlerState target) {
+    public void ApplyOnCastSpecialEffect(ref BattlerState user, ref BattlerState target)
+    {
         return;
     }
-    public float damageCalc(ref BattlerState user, ref BattlerState target) {
-        return Mathf.Round(this.power*user.getAlteredStats().getAttack()*(100f/(target.getAlteredStats().getDefense()+100f)));
+    public float damageCalc(ref BattlerState user, ref BattlerState target)
+    {
+        return Mathf.Round(this.power * user.getAlteredStats().getAttack() * (100f / (target.getAlteredStats().getDefense() + 100f)));
     }
 
-    public void playSound() {
-        if (this.sound!="")
-        AudioManager.instance.Play(this.sound+Random.Range(1,3));
+    public void playSound()
+    {
+        if (this.sound != "")
+        {
+            string sound = this.sound + Random.Range(1, 3);
+            Debug.Log(sound);
+            AudioManager.instance.Play(sound);
+        }
     }
 }
