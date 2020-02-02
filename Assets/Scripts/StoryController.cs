3be505c8e20fb1b8617f2e0e683a9c82665716db
Chad Turner <chadwickpturner@gmail.com>
2020-02-02 12:41:39 -0800

    More story

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -18,7 +18,11 @@ public class StoryController : MonoBehaviour
         "DO NOT RESIST.",
         "DO NOT RESIST.",
         "RESISTANCE WILL BE MET WITH LETHAL FORCE.",
-        "..."
+        "...",
+        "BROKEN ROBOT: ...HELP ... ME...",
+        "PUT ME BACK TOGETHER.",
+        "I CAN FIGHT.",
+        "TOGETHER WE CAN ESCAPE THIS PRISON.",
     };
     [SerializeField] public Sprite[] imgPerPanel;
     float t = 0;
@@ -30,26 +34,22 @@ public class StoryController : MonoBehaviour
 
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && currentStorySection != 9)
         {
             AudioManager.instance.Play("select");
-            if (currentStorySection == storySections.Length)
-            {
-                AudioManager.instance.Stop("siren");
-                FadeIn.GetComponent<Animator>().Play("FadeToBlack");
-            }
-            else
-            {
-                ProgressStory();
-            }
+            ProgressStory();
         }
 
-        if (currentStorySection >= storySections.Length)
+        if (currentStorySection == 9)
         {
-            currentStorySection = storySections.Length;
             t += Time.deltaTime;
-            if (t > 2)
-                SceneManager.LoadScene(nextScene);
+            if (t > 1)
+                FadeIn.GetComponent<Animator>().Play("FadeToBlack");
+            if (t > 2.5)
+            {
+                ProgressStory();
+                AudioManager.instance.Play("thud2");
+            }
         }
     }
 
@@ -63,9 +63,20 @@ public class StoryController : MonoBehaviour
         {
             AudioManager.instance.Play("siren");
         }
+        if (currentStorySection == storySections.Length)
+        {
+            SceneManager.LoadScene(nextScene);
+            return;
+        }
+
         storyText.GetComponent<Text>().text = storySections[currentStorySection];
         storyPicture.GetComponent<Image>().sprite = imgPerPanel[currentStorySection];
         currentStorySection++;
+        if (currentStorySection == 9)
+        {
+            AudioManager.instance.Stop("siren");
+            AudioManager.instance.Play("thud1");
+        }
     }
 
 }
