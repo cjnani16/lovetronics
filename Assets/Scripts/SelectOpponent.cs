e2e61544d8043b12e4a5ce739f6f7de037b00bf7
Chad Turner <chadwickpturner@gmail.com>
2020-02-02 14:40:09 -0800

    Fix sprite sizes

--- Assets/Scripts/SelectOpponent.cs
+++ Assets/Scripts/SelectOpponent.cs
@@ -10,18 +10,19 @@ public class SelectOpponent : MonoBehaviour
     List<string> enemyNames;
     public GameObject EnemyPanelPrefab, CardsParent;
     public Sprite defaultSprite;
-    int index=0;
+    int index = 0;
     float initialX;
     GameObject LButton, RButton;
 
-    BattlerState AssembleAnEnemy(float multiplier) {
-        int i = Random.Range(0,3);
+    BattlerState AssembleAnEnemy(float multiplier)
+    {
+        int i = Random.Range(0, 3);
         Hardware enemy_chassis = GameManager.manager.GetInventory().getAllHardwareOfCategory(Hardware.Category.CHASSIS)[i];
-        i = Random.Range(0,3);
+        i = Random.Range(0, 3);
         Hardware enemy_locomotion = GameManager.manager.GetInventory().getAllHardwareOfCategory(Hardware.Category.LOCOMOTION)[i];
-        i = Random.Range(0,3);
+        i = Random.Range(0, 3);
         Hardware enemy_equipment = GameManager.manager.GetInventory().getAllHardwareOfCategory(Hardware.Category.EQUIPMENT)[i];
-        
+
 
         List<Hardware> hwList = new List<Hardware>();
         hwList.Add(enemy_chassis);
@@ -56,38 +57,43 @@ public class SelectOpponent : MonoBehaviour
         CreateEnemyPanels();
     }
 
-    void setOpponent() {
+    void setOpponent()
+    {
         GameManager.manager.setNextOpponent(enemies[index]);
     }
 
-    void SetupButtons() {
+    void SetupButtons()
+    {
         LButton = transform.Find("LButton").gameObject;
         RButton = transform.Find("RButton").gameObject;
-        
-        RButton.GetComponent<Button>().onClick.AddListener(()=>{index++; setOpponent(); });
-        LButton.GetComponent<Button>().onClick.AddListener(()=>{index--; setOpponent(); });
+
+        RButton.GetComponent<Button>().onClick.AddListener(() => { index++; setOpponent(); });
+        LButton.GetComponent<Button>().onClick.AddListener(() => { index--; setOpponent(); });
     }
 
-    void UpdateButtons() {
-        LButton.SetActive(index>0);
-        RButton.SetActive(index<enemies.Count-1);
+    void UpdateButtons()
+    {
+        LButton.SetActive(index > 0);
+        RButton.SetActive(index < enemies.Count - 1);
     }
 
-    void CreateEnemyPanels() {
+    void CreateEnemyPanels()
+    {
         initialX = CardsParent.transform.position.x;
-        for(int i=0; i<enemies.Count; i++) {
-            GameObject thisPanel = GameObject.Instantiate(EnemyPanelPrefab, CardsParent.transform.position + (new Vector3(0 + 1200*i, 90)), Quaternion.identity);
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            GameObject thisPanel = GameObject.Instantiate(EnemyPanelPrefab, CardsParent.transform.position + (new Vector3(0 + 1200 * i, 90)), Quaternion.identity);
             thisPanel.transform.SetParent(CardsParent.transform);
 
             thisPanel.transform.Find("BotName").GetComponent<Text>().text = enemyNames[i];
             Sprite loadedSprite = GameManager.manager.getSprite(enemies[i]);
-            thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().sprite = (loadedSprite!=null)?loadedSprite:defaultSprite;
+            thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().sprite = (loadedSprite != null) ? loadedSprite : defaultSprite;
 
             float prevHeight = thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().GetComponent<RectTransform>().sizeDelta.y;
             thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().GetComponent<RectTransform>().sizeDelta = thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().GetComponent<Image>().sprite.rect.size;
-            float moveDownAmt = prevHeight-thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().GetComponent<RectTransform>().sizeDelta.y;
+            float moveDownAmt = prevHeight - thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().GetComponent<RectTransform>().sizeDelta.y;
 
-            thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().transform.parent.position -= new Vector3(0,moveDownAmt/2);
+            thisPanel.transform.Find("EnemyBotImage").GetComponent<Image>().transform.position -= new Vector3(0, moveDownAmt / 2);
 
 
             string statStr = "HP: " + enemies[i].getAlteredStats().getMaxHealth()
@@ -97,21 +103,26 @@ public class SelectOpponent : MonoBehaviour
             + "\nRGN: " + (enemies[i].getAlteredStats().getCoolantRegen());
 
             thisPanel.transform.Find("Stats").GetComponent<Text>().text = statStr;
-            
+
             thisPanel.transform.Find("HardwareIcons").Find("EquipmentIcon").GetComponent<Image>().sprite = enemies[i].hardware[0].sprite;
             thisPanel.transform.Find("HardwareIcons").Find("ChassisIcon").GetComponent<Image>().sprite = enemies[i].hardware[1].sprite;
             thisPanel.transform.Find("HardwareIcons").Find("LocomotionIcon").GetComponent<Image>().sprite = enemies[i].hardware[2].sprite;
         }
     }
 
-    void UpdateEnemyPanels() {
-        float targetX = -1200*index + initialX;
-        if (CardsParent.transform.position.x - targetX > 250) {
-            CardsParent.transform.position -= new Vector3(100,0);
-        } else if ( CardsParent.transform.position.x - targetX < -250) {
-            CardsParent.transform.position += new Vector3(100,0);
+    void UpdateEnemyPanels()
+    {
+        float targetX = -1200 * index + initialX;
+        if (CardsParent.transform.position.x - targetX > 250)
+        {
+            CardsParent.transform.position -= new Vector3(100, 0);
         }
-        else {
+        else if (CardsParent.transform.position.x - targetX < -250)
+        {
+            CardsParent.transform.position += new Vector3(100, 0);
+        }
+        else
+        {
             CardsParent.transform.position = new Vector3(targetX, CardsParent.transform.position.y);
         }
     }
